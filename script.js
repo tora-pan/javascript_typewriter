42755049f990d5c8910d9b5cf9261bf819d01a7b
tora_pan <travis.pandos@gmail.com>
2021-07-14 16:00:40 -0700

    remove inital animation in to fix overflow for now

--- script.js
+++ script.js
@@ -67,23 +67,22 @@ const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav-links");
   const navLinks = document.querySelectorAll(".nav-links li");
-    //toggle nav
+  //toggle nav
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-active");
-    
     //Animate links sliding in
-    navLinks.forEach((link,index) =>{
-        if(link.style.animation){
-            link.style.animation = '';
-        }else{
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + .4}s`;
-        }
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.1
+        }s`;
+      }
     });
     //toggle hamburger
-    burger.classList.toggle('toggle');
+    burger.classList.toggle("toggle");
   });
-
-
 };
 
 navSlide();
