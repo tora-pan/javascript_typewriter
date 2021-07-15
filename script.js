347fa5f35c685a08dfc8554ed7a4330bd073bf10
tora_pan <travis.pandos@gmail.com>
2021-07-14 20:18:21 -0700

    stop css transition from triggering during screen resize

--- script.js
+++ script.js
@@ -86,3 +86,13 @@ const navSlide = () => {
 };
 
 navSlide();
+
+//stop animation on resizing screen
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
